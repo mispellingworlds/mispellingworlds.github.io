9c03280d16cae9d25f444ea9fba83c6ddd3ac71e
Nicolae <munteanunicxlae@gmail.com>
2026-04-16 17:07:55 +0200

    image mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import { defineConfig, fontProviders} from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
   site: 'https://mispellingworlds.github.io',
+  integrations: [mdx()],
 })
