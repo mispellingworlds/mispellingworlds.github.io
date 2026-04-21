78df39003164144de8420dfc416d2562f55659a7
Nicolae <munteanunicxlae@gmail.com>
2026-04-21 22:14:08 +0200

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig, fontProviders} from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://mispellingworlds.github.io',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 })
