f780e5faf4ed55451dd5b55fd0a48f36a6363e69
Nicolae <munteanunicxlae@gmail.com>
2026-08-09 16:04:56 +0200

    Update inscription.tsx

--- components/inscription.tsx
+++ components/inscription.tsx
@@ -12,7 +12,7 @@ type InscriptionProps = {
  */
 export default function Inscription({ it, en }: InscriptionProps) {
   return (
-    <div className="my-8 grid grid-cols-2 items-center gap-x-4 text-[1.4rem] [hyphens:none] max-md:text-[1.1rem]">
+    <div className="my-8 grid grid-cols-1 items-center gap-x-4 gap-y-4 text-[1.4rem] [hyphens:none] max-md:text-[1.1rem] md:grid-cols-2 md:gap-y-0">
       <div className="whitespace-pre-line text-center">{it}</div>
       <div className="whitespace-pre-line text-center text-grey-link">{en}</div>
     </div>
